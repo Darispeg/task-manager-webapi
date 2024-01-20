79f1b3af93db9a2e6cc94cf9fdb5cbd054ef2e1c
Dennis <darispegdennis@gmail.com>
2024-01-20 05:32:29 -0400

    TM-03 #time 15min: Update Cross origin

--- task-manager/src/main/java/com/example/taskmanager/category/CategoryController.java
+++ task-manager/src/main/java/com/example/taskmanager/category/CategoryController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(value = "*")
 @RequestMapping("/api/categories")
 public class CategoryController {
     @Autowired
--- task-manager/src/main/java/com/example/taskmanager/status/StatusController.java
+++ task-manager/src/main/java/com/example/taskmanager/status/StatusController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(value = "*")
 @RequestMapping("/api/status")
 public class StatusController {
     @Autowired
